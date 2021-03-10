8dd6f2dadcb937248065d8da5f21442745a631ae
William Hicks <whicks@nvidia.com>
2021-03-10 13:54:01 -0500

    Normalize working directory in build.sh

--- ops/build.sh
+++ ops/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+BUILD_DIR="${BUILD_DIR:-$REPO_ROOT/build}"
+pushd "$BUILD_DIR" > /dev/null 2>&1
 cmake -DCMAKE_INSTALL_PREFIX:PATH="${PWD}/install" ..
 make install
+popd > /dev/null 2>&1
