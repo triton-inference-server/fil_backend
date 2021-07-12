6bc538eb8921d86bb5c0a51b660cdc8ef9b28d8b
William Hicks <whicks@nvidia.com>
2021-07-12 14:06:10 -0400

    Remove redundant libs in move_deps.sh

--- ops/move_deps.sh
+++ ops/move_deps.sh
@@ -15,4 +15,28 @@ then
 fi
 
 deps_list=$(ldd "${FIL_LIB}" | grep "${CONDA_LIB_DIR}" | awk '{print $3}' | grep -v libcudart\.so)
+echo $deps_list
 cp $deps_list "${LIB_DIR}"
+
+patchelf --set-rpath "$LIB_DIR" "$FIL_LIB"
+
+get_missing() {
+  ldd "${FIL_LIB}" | grep 'not found' | wc -l
+}
+
+redundant="${LIB_DIR}/redundant"
+mkdir "${redundant}"
+
+count=$(get_missing)
+
+for lib in "${LIB_DIR}"/*.so*
+do
+  tmp_path="${redundant}/$(basename $lib)"
+  mv "${lib}" "${tmp_path}"
+  if [ $(get_missing) -gt $count ]
+  then
+    mv "${tmp_path}" "${lib}"
+  fi
+done
+
+rm -r "${redundant}"
