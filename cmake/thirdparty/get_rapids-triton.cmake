d3d24eb5544e716c9128b84a81af52ca8950637a
William Hicks <whicks@nvidia.com>
2021-10-21 16:11:12 -0400

    Pin to RAPIDS-Triton 21.10

--- cmake/thirdparty/get_rapids-triton.cmake
+++ cmake/thirdparty/get_rapids-triton.cmake
@@ -42,5 +42,5 @@ endfunction()
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_rapids_triton(VERSION    21.10
                         FORK       rapidsai
-                        PINNED_TAG main
+                        PINNED_TAG branch-21.10
                         )
