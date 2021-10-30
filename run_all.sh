519cc5bf76aba687bfeb888cfdfa03c61f401940
William Hicks <whicks@nvidia.com>
2021-10-30 17:59:43 -0400

    Remove no-cache build

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker build --no-cache -t triton_fil -f ops/Dockerfile .
+docker build -t triton_fil -f ops/Dockerfile .
 docker build -t triton_test -f qa/Dockerfile .
 gpus=${CUDA_VISIBLE_DEVICES:-all}
 docker run -v $PWD/logs:/logs --name triton_test --rm --gpus $gpus triton_test
