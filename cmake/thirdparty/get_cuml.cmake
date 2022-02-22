ecc2d38e5838ef97510488de7492c47b6526b7be
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-02-22 15:51:34 -0800

    Use cuML 22.04

--- cmake/thirdparty/get_cuml.cmake
+++ cmake/thirdparty/get_cuml.cmake
@@ -37,7 +37,7 @@ endfunction()
 # Change pinned tag here to test a commit in CI
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
-find_and_configure_cuml(VERSION    21.12
+find_and_configure_cuml(VERSION    22.04
                         FORK       rapidsai
-                        PINNED_TAG branch-21.12
+                        PINNED_TAG branch-22.04
                         )
