72c7162190c4805920bc92a464ad3d348bc0525e
William Hicks <whicks@nvidia.com>
2022-02-25 18:32:06 -0500

    Switch to convex hull benchmark graphs

--- qa/collate_benchmarks.py
+++ qa/collate_benchmarks.py
@@ -4,6 +4,9 @@ import re
 import sys
 
 import cudf
+
+from scipy.spatial import ConvexHull
+
 try:
     import matplotlib.pyplot as plt
 except ImportError:
@@ -38,17 +41,33 @@ def collate_raw_data(benchmark_dir):
         all_data.append(cudf.concat([annotations, data], axis=1))
     return cudf.concat(all_data, axis=0, ignore_index=True)
 
+def pts_to_line(pt1, pt2):
+    slope = (pt2[1] - pt1[1]) / (pt2[0] - pt1[0])
+    intercept = pt1[1] - slope * pt1[0]
+    return (slope, intercept)
 
-def plot_lat_tp(data):
+def scatter_to_hull(pts):
+    hull = ConvexHull(pts)
+    pts = pts[hull.vertices]
+    pts = pts[pts[:, 0].argsort(), :]
+    slope, intercept = pts_to_line(pts[0, :], pts[-1, :])
+    filtered_pts = pts[pts[:, 1] >= slope * pts[:, 0] + intercept]
+    if len(filtered_pts) < 2 and len(pts) >= 2:
+        return pts[(0, -1), :]
+    else:
+        return filtered_pts
+
+
+def plot_lat_tp(data, latency_percentile=99):
     all_models = data['Model'].unique().to_pandas()
     plt.xscale('log')
     plt.yscale('log')
     for model in all_models:
         model_data = raw_data.loc[data['Model'] == model].to_pandas()
-        plt.scatter(
-            model_data['p99 latency'],
-            model_data['Inferences/Second']
-        )
+        hull = scatter_to_hull(model_data[
+            [f'p{latency_percentile} latency', 'Inferences/Second']
+        ].values)
+        plt.plot(hull[:, 0], hull[:, 1], '-', label=model)
     plt.title('Throughput vs. Latency (log-log)')
     plt.xlabel('p99 Latency (microseconds)')
     plt.ylabel('Throughput (samples/s)')
