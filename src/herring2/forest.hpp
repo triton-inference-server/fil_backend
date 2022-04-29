f5ee4fd462ff0914471eb685dea7564cb3995e91
William Hicks <whicks@nvidia.com>
2022-04-29 17:21:34 -0400

    Beginning implementing categorical evaluation

--- src/herring2/forest.hpp
+++ src/herring2/forest.hpp
@@ -3,36 +3,33 @@
 #include <herring2/detail/index_type.hpp>
 #include <herring2/flat_array.hpp>
 #include <herring2/gpu_support.hpp>
+#include <herring2/node_value.hpp>
 #include <herring2/tree.hpp>
 
 namespace herring {
 
-template<typename value_t, typename feature_index_t, typename offset_t, typename output_index_t, typename output_t, tree_layout layout>
+template<typename value_t, typename feature_index_t, typename offset_t, typename output_index_t, typename output_t, typename bitset_t, tree_layout layout>
 struct forest {
   using index_type = detail::index_type<DEBUG_ENABLED && !GPU_ENABLED>;
 
-  template <bool categorical, data_layout input_layout, typename input_t>
+  template <bool categorical, bool lookup, data_layout input_layout, typename input_t>
   HOST DEVICE auto evaluate_tree(
     index_type tree_index,
     index_type row_index,
     data_array<input_layout, input_t> const& input
   ) const {
-    return flat_array<array_encoding::dense>(
-      outputs_ + find_leaf<categorical>(tree_index, row_index, input),
-      output_size_
-    );
+    return get_output<lookup>(find_leaf<categorical>(tree_index, row_index, input));
   }
 
-  template <bool categorical, data_layout input_layout, typename input_t>
+  template <bool categorical, bool lookup, data_layout input_layout, typename input_t>
   HOST DEVICE auto evaluate_tree(
     index_type tree_index,
     index_type row_index,
     data_array<input_layout, input_t> const& input,
     data_array<input_layout, bool> const& missing_values
   ) const {
-    return flat_array<array_encoding::dense>(
-      outputs_ + find_leaf<categorical>(tree_index, row_index, input, missing_values),
-      output_size_
+    return get_output<lookup>(
+      find_leaf<categorical>(tree_index, row_index, input, missing_values)
     );
   }
 
@@ -40,7 +37,7 @@ struct forest {
   raw_index_t node_count_;
   offset_t* distant_offsets_;
   feature_index_t* features_;
-  value_or_index* values_;
+  node_value<value_t, output_index_t, bitset_t>* values_;
   bool* default_distant_;
 
   raw_index_t tree_count_;
@@ -51,7 +48,32 @@ struct forest {
   // Optional data (may be null)
   output_t* outputs_;
   bool* categorical_nodes_;
-  // TODO(wphicks): Add optional separate category storage for many categories
+  bitset_t* node_categories_;
+
+  template<bool lookup>
+  HOST DEVICE auto get_output(index_type leaf_index) {
+    if constexpr (lookup) {
+      return flat_array<array_encoding::dense, output_t>(
+        outputs_ + leaf_index,
+        output_size_
+      );
+    } else {
+      auto const& value = values_ + leaf_index;
+      if constexpr (std::is_same_v<value_t, output_t>) {
+        return flat_array<array_encoding::dense, output_t>(
+          &(value.value),
+          1
+        );
+      } else if constexpr (std::is_same_v<value_t, output_t>) {
+        return flat_array<array_encoding::dense, output_t>(
+          &(value.index),
+          1
+        );
+      } else {
+        // static_assert(false);
+      }
+    }
+  }
 
   template <bool categorical, data_layout input_layout, typename input_t>
   HOST DEVICE auto find_leaf(
@@ -66,12 +88,12 @@ struct forest {
     do {
       node_index_tree += offset;
       offset = tree.next_offset(
-        node_tree_index,
+        node_index_tree,
         evaluate_node<categorical>(root_index_forest + node_index_tree, row_index, input)
       );
     } while (offset != raw_index_t{});
 
-    return root_index_forest + tree_node;
+    return root_index_forest + node_index_tree;
   }
 
   template <bool categorical, data_layout input_layout, typename input_t>
@@ -88,18 +110,18 @@ struct forest {
     do {
       node_index_tree += offset;
       offset = tree.next_offset(
-        node_tree_index,
+        node_index_tree,
         evaluate_node<categorical>(root_index_forest + node_index_tree, row_index, input, missing_values)
       );
     } while (offset != raw_index_t{});
 
-    return root_index_forest + tree_node;
+    return root_index_forest + node_index_tree;
   }
 
   HOST DEVICE [[nodiscard]] auto get_tree(index_type tree_index) const {
     auto min_index = tree_offsets_[tree_index];
-    auto max_index = tree_index + 1 >= tree_counts ? node_count : tree_offsets_[tree_index + 1];
-    return tree<layout, offset_t>{distant_offsets + min_index, max_index - min_index};
+    auto max_index = tree_index + 1 >= tree_count_ ? node_count_ : tree_offsets_[tree_index + 1];
+    return tree<layout, offset_t>{distant_offsets_ + min_index, max_index - min_index};
   }
 
   template <bool categorical, data_layout input_layout, typename input_t>
@@ -109,9 +131,16 @@ struct forest {
     data_array<input_layout, input_t> const& input
   ) {
     if constexpr (!categorical) {
-      return input.at(row, features_[node_index]) < values_[node_index].value;
+      return input.at(row_index, features_[node_index]) < values_[node_index].value;
     } else {
-      // TODO(wphicks): Add categorical logic
+      auto const& categories = values_[node_index].categories;
+      if constexpr (
+        sizeof(bitset_t) > sizeof(value_t) && sizeof(bitset_t) > sizeof(output_index_t)
+      ) {
+        categories = node_categories_[values_[node_index].categories];
+      }
+      auto value = input.at(row_index, features_[node_index]);
+      // TODO(wphicks): auto categorical_value = 
     }
   }
 
@@ -123,10 +152,12 @@ struct forest {
     data_array<input_layout, bool> const& missing_values
   ) {
     auto col = features_[node_index];
-    if (missing_values.at(row, col)) {
+    if (missing_values.at(row_index, col)) {
       return default_distant_[node_index];
     } else {
       return evaluate_node(node_index, row_index, input);
     }
   }
 };
+
+}
