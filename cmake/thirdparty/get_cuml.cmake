a7d1853d455b82ba500fab0dfc65081425cd6f3b
William Hicks <whicks@nvidia.com>
2022-06-17 13:15:03 -0700

    Use cuML fork with Treelite 2.0.0

--- cmake/thirdparty/get_cuml.cmake
+++ cmake/thirdparty/get_cuml.cmake
@@ -57,7 +57,7 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_cuml(VERSION    ${RAPIDS_TRITON_MIN_VERSION_rapids_projects}
-                        FORK       rapidsai
-                        PINNED_TAG branch-${RAPIDS_DEPENDENCIES_VERSION}
+                        FORK       wphicks
+                        PINNED_TAG exp-2204_treelite_200
                         USE_TREELITE_STATIC ${TRITON_FIL_USE_TREELITE_STATIC}
                         )
