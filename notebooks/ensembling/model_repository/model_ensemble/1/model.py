9f9eb58a1fb44eab7f40efb31f93e355dfc45a4b
Erik Narvades <enarvades@berkeley.edu>
2022-08-03 11:21:00 +0000

    Added python model to notebook

--- notebooks/ensembling/model_repository/model_ensemble/1/model.py
+++ notebooks/ensembling/model_repository/model_ensemble/1/model.py
@@ -1,27 +1,11 @@
 import triton_python_backend_utils as pb_utils
+from torch.utils.dlpack import from_dlpack
 import json
 import asyncio
 
 class TritonPythonModel:
-    """Your Python model must use the same class name. Every Python model
-    that is created must have "TritonPythonModel" as the class name.
-    """
-
     def initialize(self, args):
-        """`initialize` is called only once when the model is being loaded.
-        Implementing `initialize` function is optional. This function allows
-        the model to intialize any state associated with this model.
-        Parameters
-        ----------
-        args : dict
-          Both keys and values are strings. The dictionary keys and values are:
-          * model_config: A JSON string containing the model configuration
-          * model_instance_kind: A string containing model instance kind
-          * model_instance_device_id: A string containing model instance device ID
-          * model_repository: Model repository path
-          * model_version: Model version
-          * model_name: Model name
-        """
+
 
         # You must parse model_config. JSON string is not parsed here
         self.model_config = json.loads(args['model_config'])
@@ -36,25 +20,6 @@ class TritonPythonModel:
     # You must add the Python 'async' keyword to the beginning of `execute`
     # function if you want to use `async_exec` function.
     async def execute(self, requests):
-        """`execute` must be implemented in every Python model. `execute`
-        function receives a list of pb_utils.InferenceRequest as the only
-        argument. This function is called when an inference request is made
-        for this model. Depending on the batching configuration (e.g. Dynamic
-        Batching) used, `requests` may contain multiple requests. Every
-        Python model, must create one pb_utils.InferenceResponse for every
-        pb_utils.InferenceRequest in `requests`. If there is an error, you can
-        set the error argument when creating a pb_utils.InferenceResponse
-        Parameters
-        ----------
-        requests : list
-          A list of pb_utils.InferenceRequest
-        Returns
-        -------
-        list
-          A list of pb_utils.InferenceResponse. The length of this list must
-          be the same as `requests`
-        """
-
         output0_dtype = self.output0_dtype
 
         responses = []
@@ -66,7 +31,7 @@ class TritonPythonModel:
 
             # List of awaitables containing inflight inference responses.
             inference_response_awaits = []
-            for model_name in ['model_logistic-cpu', 'model_oversample-cpu', 'model_RFC-cpu']:
+            for model_name in ['model_logistic', 'model_oversample', 'model_RFC']:
                 # Create inference request object
                 infer_request = pb_utils.InferenceRequest(
                     model_name=model_name,
@@ -92,19 +57,19 @@ class TritonPythonModel:
                     raise pb_utils.TritonModelException(
                         infer_response.error().message())
 
-            # Get the OUTPUT0 from the "pytorch" model inference response
-            logistic_output0_tensor = pb_utils.get_output_tensor_by_name(
+            logistic_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[0], "output__0")
+            logistic_tensor = from_dlpack(logistic_tensor.to_dlpack())
 
-            # Get the OUTPUT1 from the "addsub" model inference response
-            oversample_output1_tensor = pb_utils.get_output_tensor_by_name(
+            oversample_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[1], "output__0")
+            oversample_tensor = from_dlpack(oversample_tensor.to_dlpack())
             
-            RFC_output2_tensor = pb_utils.get_output_tensor_by_name(
+            RFC_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[2], "output__0")
+            RFC_tensor = from_dlpack(RFC_tensor.to_dlpack())
             
-            ensembled = (logistic_output0_tensor.as_numpy() + oversample_output1_tensor.as_numpy() + RFC_output2_tensor.as_numpy()) / 3
-
+            ensembled = (logistic_tensor.as_numpy() + oversample_tensor.as_numpy() + RFC_tensor.as_numpy()) / 3
             ensembled_tensor = pb_utils.Tensor("output__0", ensembled.astype(output0_dtype))
 
             # Create InferenceResponse. You can set an error here in case
@@ -127,8 +92,4 @@ class TritonPythonModel:
         return responses
 
     def finalize(self):
-        """`finalize` is called only once when the model is being unloaded.
-        Implementing `finalize` function is OPTIONAL. This function allows
-        the model to perform any necessary clean ups before exit.
-        """
         print('Cleaning up...')
