a24cdf79c5008092ba860a80b2eb88925c86aba1
Erik Narvades <enarvades@berkeley.edu>
2022-08-11 16:32:32 +0000

    Rough draft

--- notebooks/ensembling/model_repository/model_ensemble/1/model.py
+++ notebooks/ensembling/model_repository/model_ensemble/1/model.py
@@ -1,27 +1,17 @@
-import triton_python_backend_utils as pb_utils
-from torch.utils.dlpack import from_dlpack
-import json
 import asyncio
+import json
+import numpy as np
+import triton_python_backend_utils as pb_utils
+
 
 class TritonPythonModel:
     def initialize(self, args):
-
-
         # You must parse model_config. JSON string is not parsed here
         self.model_config = json.loads(args['model_config'])
-
-        # Get OUTPUT0 configuration
-        self.output0_config = pb_utils.get_output_config_by_name(
-            self.model_config, "output__0")
-        
-        self.output0_dtype = pb_utils.triton_string_to_numpy(
-            self.output0_config['data_type'])
 
     # You must add the Python 'async' keyword to the beginning of `execute`
     # function if you want to use `async_exec` function.
     async def execute(self, requests):
-        output0_dtype = self.output0_dtype
-
         responses = []
         # Every Python backend must iterate over everyone of the requests
         # and create a pb_utils.InferenceResponse for each of them.
@@ -59,30 +49,17 @@ class TritonPythonModel:
 
             logistic_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[0], "output__0")
-            logistic_tensor = from_dlpack(logistic_tensor.to_dlpack())
-
+                        
             oversample_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[1], "output__0")
-            oversample_tensor = from_dlpack(oversample_tensor.to_dlpack())
             
             RFC_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[2], "output__0")
-            RFC_tensor = from_dlpack(RFC_tensor.to_dlpack())
             
-            ensembled = (logistic_tensor.as_numpy() + oversample_tensor.as_numpy() + RFC_tensor.as_numpy()) / 3
-            ensembled_tensor = pb_utils.Tensor("output__0", ensembled.astype(output0_dtype))
+            ensembled = (np._from_dlpack(logistic_tensor.to_dlpack()) + np._from_dlpack(oversample_tensor.to_dlpack()) + np._from_dlpack(RFC_tensor.to_dlpack())) / 3
+            ensembled = ensembled.__dlpack__()
+            ensembled_tensor = pb_utils.Tensor.from_dlpack("output__0", ensembled)
 
-            # Create InferenceResponse. You can set an error here in case
-            # there was a problem with handling this inference request.
-            # Below is an example of how you can set errors in inference
-            # response:
-            #
-            # pb_utils.InferenceResponse(
-            #    output_tensors=..., TritonError("An error occured"))
-            #
-            # Because the infer_response of the models contains the final
-            # outputs with correct output names, we can just pass the list
-            # of outputs to the InferenceResponse object.
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[ensembled_tensor])
             responses.append(inference_response)
