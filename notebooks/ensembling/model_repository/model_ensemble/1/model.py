e2615a294fa799ed4815a06ce6a8d34ef5eb32a9
Erik Narvades <enarvades@berkeley.edu>
2022-08-19 13:57:53 +0000

    Added GPU support

--- notebooks/ensembling/model_repository/model_ensemble/1/model.py
+++ notebooks/ensembling/model_repository/model_ensemble/1/model.py
@@ -1,17 +1,27 @@
-import asyncio
-import json
-import numpy as np
 import triton_python_backend_utils as pb_utils
-
+import json
+import asyncio
+from torch.utils.dlpack import from_dlpack
 
 class TritonPythonModel:
     def initialize(self, args):
+
+
         # You must parse model_config. JSON string is not parsed here
         self.model_config = json.loads(args['model_config'])
+
+        # Get OUTPUT0 configuration
+        self.output0_config = pb_utils.get_output_config_by_name(
+            self.model_config, "output__0")
+        
+        self.output0_dtype = pb_utils.triton_string_to_numpy(
+            self.output0_config['data_type'])
 
     # You must add the Python 'async' keyword to the beginning of `execute`
     # function if you want to use `async_exec` function.
     async def execute(self, requests):
+        output0_dtype = self.output0_dtype
+
         responses = []
         # Every Python backend must iterate over everyone of the requests
         # and create a pb_utils.InferenceResponse for each of them.
@@ -49,16 +59,40 @@ class TritonPythonModel:
 
             logistic_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[0], "output__0")
-                        
+
             oversample_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[1], "output__0")
             
             RFC_tensor = pb_utils.get_output_tensor_by_name(
                 inference_responses[2], "output__0")
-            
-            ensembled = (np._from_dlpack(logistic_tensor.to_dlpack()) + np._from_dlpack(oversample_tensor.to_dlpack()) + np._from_dlpack(RFC_tensor.to_dlpack())) / 3
-            ensembled = ensembled.__dlpack__()
-            ensembled_tensor = pb_utils.Tensor.from_dlpack("output__0", ensembled)
+
+            logistic_tensor = logistic_tensor.to_dlpack()
+            oversample_tensor = oversample_tensor.to_dlpack()
+            RFC_tensor = RFC_tensor.to_dlpack()
+
+            class DLPack:
+                def __init__(self, tensor):
+                    self.tensor = tensor
+                
+                def __dlpack__(self):
+                    return self.tensor
+                
+                def __dlpack_device__(self):
+                    return self.tensor.memory_type_id
+
+            logistic_tensor = DLPack(logistic_tensor)
+            oversample_tensor = DLPack(oversample_tensor)
+            RFC_tensor = DLPack(RFC_tensor)
+
+            def pb_tensor_to_numpy(pb_tensor):
+                if pb_tensor.is_cpu():
+                    return pb_tensor.as_numpy()
+                else:
+                    pytorch_tensor = from_dlpack(pb_tensor)
+                return pytorch_tensor.cpu().numpy()
+
+            ensemble = (pb_tensor_to_numpy(logistic_tensor) + pb_tensor_to_numpy(oversample_tensor) + pb_tensor_to_numpy(RFC_tensor)) / 3
+            ensembled_tensor = pb_utils.Tensor("output__0", ensemble)
 
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[ensembled_tensor])
