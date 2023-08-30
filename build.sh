f497a2ca3ba80f125832ae7e8f2f4906d43b5c79
William Hicks <whicks@nvidia.com>
2023-08-30 14:31:35 -0400

    Update to 23.08 Triton base container

--- build.sh
+++ build.sh
@@ -190,7 +190,7 @@ if [ -z $TRITON_VERSION ] && [ $HOST_BUILD -eq 1 ]
 then
   # Must use a version compatible with a released backend image in order to
   # test a host build, so default to latest release branch rather than main
-  TRITON_VERSION=23.07
+  TRITON_VERSION=23.08
 fi
 
 if [ ! -z $TRITON_VERSION ]
