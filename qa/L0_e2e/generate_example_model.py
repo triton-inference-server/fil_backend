42857004d448ed057ae2b3165adc631db8158889
Hyunsu Cho <phcho@nvidia.com>
2024-07-01 18:01:28 -0700

    Add test coverage

--- qa/L0_e2e/generate_example_model.py
+++ qa/L0_e2e/generate_example_model.py
@@ -87,6 +87,10 @@ def train_lightgbm_classifier(data, labels, depth=25, trees=100, classes=2):
     """Train LightGBM classification model"""
     if lgb is None:
         raise RuntimeError("LightGBM could not be imported")
+    # Use float64 columns, to test if FIL can handle LightGBM models with
+    # float64 thresholds
+    cols = data.select_dtypes(include="number").columns
+    data[cols] = data[cols].astype("float64")
     lgb_data = lgb.Dataset(data, label=labels)
 
     if classes <= 2:
