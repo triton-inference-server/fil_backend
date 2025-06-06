c2fa763d472712737815646ba508d9fa3663ba4a
Philip Hyunsu Cho <phcho@nvidia.com>
2025-06-05 20:54:13 -0700

    Provide 25.06 version bumps (#433)
    * Provide 25.06 version bumps
    
    * Use Rapids 25.04
    
    * Use correct Rapids-CMake

--- build.sh
+++ build.sh
@@ -190,7 +190,7 @@ if [ -z $TRITON_VERSION ] && [ $HOST_BUILD -eq 1 ]
 then
   # Must use a version compatible with a released backend image in order to
   # test a host build, so default to latest release branch rather than main
-  TRITON_VERSION=23.09
+  TRITON_VERSION=25.05
 fi
 
 if [ ! -z $TRITON_VERSION ]
