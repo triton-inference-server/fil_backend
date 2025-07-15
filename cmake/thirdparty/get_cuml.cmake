01e88e51a1e2376ecbc5103c69eb244e3e7e74f3
Hyunsu Cho <phcho@nvidia.com>
2025-07-14 21:03:37 -0700

    Provide 25.07 version bumps

--- cmake/thirdparty/get_cuml.cmake
+++ cmake/thirdparty/get_cuml.cmake
@@ -55,8 +55,8 @@ endfunction()
 # Change pinned tag here to test a commit in CI
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
-find_and_configure_cuml(VERSION    25.04
+find_and_configure_cuml(VERSION    25.06
                         FORK       rapidsai
-                        PINNED_TAG branch-25.04
+                        PINNED_TAG 7d13c6a194a77b4d6849cd44631678261eed0f7f
                         USE_TREELITE_STATIC ${TRITON_FIL_USE_TREELITE_STATIC}
                         )
