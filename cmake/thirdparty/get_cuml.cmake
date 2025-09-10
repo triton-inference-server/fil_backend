daa1229317994bde8e98d28cc60c83c3ef9dd0c2
Hyunsu Cho <chohyu01@cs.washington.edu>
2025-09-10 20:02:07 +0000

    Update dependency versions

--- cmake/thirdparty/get_cuml.cmake
+++ cmake/thirdparty/get_cuml.cmake
@@ -56,8 +56,8 @@ endfunction()
 # Change pinned tag here to test a commit in CI
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
-find_and_configure_cuml(VERSION    25.06
+find_and_configure_cuml(VERSION    25.08
                         FORK       rapidsai
-                        PINNED_TAG 7d13c6a194a77b4d6849cd44631678261eed0f7f
+                        PINNED_TAG 00094f7e4e4b5da3a968d193a4da6085fa38f11b
                         USE_TREELITE_STATIC ${TRITON_FIL_USE_TREELITE_STATIC}
                         )
