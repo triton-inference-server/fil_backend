d53144e783e5fcd1a20c46824db0dbf1aede9244
Hyunsu Cho <phcho@nvidia.com>
2025-09-10 22:20:30 -0700

    Ensure device_id is correctly set

--- src/gpu_forest_model.h
+++ src/gpu_forest_model.h
@@ -17,17 +17,17 @@
 #pragma once
 
 #include <cuda_runtime_api.h>
-#include <cuml/fil/detail/raft_proto/device_type.hpp>
-#include <cuml/fil/forest_model.hpp>
-#include <cuml/fil/infer_kind.hpp>
-#include <cuml/fil/treelite_importer.hpp>
+#include <detail/postprocess_gpu.h>
 #include <fil_config.h>
 #include <forest_model.h>
 #include <names.h>
 #include <tl_model.h>
-#include <detail/postprocess_gpu.h>
 
 #include <cstddef>
+#include <cuml/fil/detail/raft_proto/device_type.hpp>
+#include <cuml/fil/forest_model.hpp>
+#include <cuml/fil/infer_kind.hpp>
+#include <cuml/fil/treelite_importer.hpp>
 #include <memory>
 #include <raft/core/handle.hpp>
 #include <rapids_triton/memory/buffer.hpp>
@@ -42,12 +42,11 @@ struct ForestModel<rapids::DeviceMemory> {
       device_id_t device_id, cudaStream_t stream,
       std::shared_ptr<TreeliteModel> tl_model)
       : device_id_{device_id}, raft_handle_{stream}, tl_model_{tl_model},
-        fil_forest_{[this]() {
+        fil_forest_{[this, device_id, stream]() {
           auto config = tl_model_->config();
           auto result = ML::fil::import_from_treelite_handle(
-            tl_model_->handle(), detail::name_to_fil_layout(config.layout), 128,
-            false, raft_proto::device_type::gpu, device_id_,
-            raft_handle_.get_stream());
+              tl_model_->handle(), detail::name_to_fil_layout(config.layout),
+              128, false, raft_proto::device_type::gpu, device_id, stream);
           return result;
         }()}
   {
@@ -66,15 +65,14 @@ struct ForestModel<rapids::DeviceMemory> {
   {
     // Create non-owning Buffer to same memory as `output`
     auto output_buffer = rapids::Buffer<float>{
-      output.data(), output.size(), output.mem_type(), output.device(),
-      output.stream()};
+        output.data(), output.size(), output.mem_type(), output.device(),
+        output.stream()};
     auto output_size = output.size();
     // FIL expects buffer of size samples * num_classes for multi-class
     // classifiers, but output buffer may be smaller, so we need a temporary
     // buffer
     auto const num_classes = tl_model_->num_classes();
-    if (!predict_proba && tl_model_->config().output_class &&
-        num_classes > 1) {
+    if (!predict_proba && tl_model_->config().output_class && num_classes > 1) {
       output_size = samples * num_classes;
       if (output_size != output.size()) {
         // If expected output size is not the same as the size of `output`,
@@ -85,19 +83,17 @@ struct ForestModel<rapids::DeviceMemory> {
     }
     fil_forest_.predict(
         raft_proto::handle_t{raft_handle_}, output_buffer.data(),
-        const_cast<float*>(input.data()), samples,
-        raft_proto::device_type::gpu,
-        raft_proto::device_type::gpu,
-        ML::fil::infer_kind::default_kind,
+        const_cast<float*>(input.data()), samples, raft_proto::device_type::gpu,
+        raft_proto::device_type::gpu, ML::fil::infer_kind::default_kind,
         tl_model_->config().chunk_size);
 
     if (!predict_proba && tl_model_->config().output_class) {
       if (num_classes > 1) {
         class_encoder_.argmax_for_multiclass(
-          output, output_buffer, samples, num_classes);
+            output, output_buffer, samples, num_classes);
       } else if (num_classes == 1) {
         class_encoder_.threshold_inplace(
-          output, samples, tl_model_->config().threshold);
+            output, samples, tl_model_->config().threshold);
       }
     }
   }
