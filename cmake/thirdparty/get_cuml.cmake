8b759aa168c03c8c2f92d12e1508c8ce9537abda
Hyunsu Cho <phcho@nvidia.com>
2025-09-12 02:08:05 -0700

    Remove CUDA 13 patches

--- cmake/thirdparty/get_cuml.cmake
+++ cmake/thirdparty/get_cuml.cmake
@@ -31,7 +31,6 @@ function(find_and_configure_cuml)
             GIT_REPOSITORY https://github.com/${PKG_FORK}/cuml.git
             GIT_TAG        ${PKG_PINNED_TAG}
             SOURCE_SUBDIR  cpp
-            PATCHES        "./patches/cuml.patch"
             OPTIONS
               "BUILD_CUML_C_LIBRARY OFF"
               "BUILD_CUML_CPP_LIBRARY ON"
